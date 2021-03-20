65ad9607c2dbf7337db22966706187eaf470ff6b
Johnson Okorie <jokorie@inlaks.com>
2021-03-20 21:26:03 +0100

    ok

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const getRoutes = require('./routes');
 const PORT = process.env.PORT || 5000;
-var cors = require('cors');;
+var cors = require('cors');
 
 
 const app = express();
